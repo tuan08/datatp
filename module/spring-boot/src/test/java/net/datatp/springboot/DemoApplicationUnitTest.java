14e6379ca796e85f92b30df3cbf4da6c199beaf3
Tuan Nguyen <tuan08@gmail.com>
2016-06-29 15:16:13 +0700

    refractor es json mapping to datatp. Create mulltiple spring app sample with different classloader

--- module/spring-boot/src/test/java/net/datatp/springboot/DemoApplicationUnitTest.java
+++ module/spring-boot/src/test/java/net/datatp/springboot/DemoApplicationUnitTest.java
@@ -1,15 +1,19 @@
-package net.datatp;
+package net.datatp.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import net.datatp.DemoApplication;
+import net.datatp.springboot.app1.Application1;
+import net.datatp.springboot.app2.Application2;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(
+  classes = {Application1.class, Application2.class}
+)
 public class DemoApplicationUnitTest {
+  
   @Test
   public void contextLoads() {
   }
